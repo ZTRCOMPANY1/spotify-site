8224fe206e037cb0d0a96665f6086ff3832df67d
ZTRCOMPANY1 <ztrcompany1@gmail.com>
2025-01-28 01:48:34 -0300

    super atualização

--- script.js
+++ script.js
@@ -47,11 +47,14 @@ if (document.getElementById("user-name")) {
 // Função para carregar categorias e músicas em destaque
 if (document.getElementById("categories")) {
     const categories = [
-        { name: "Pop", img: "assets/sla.png" },
-        { name: "Rock", img: "assets/rock.jpg" },
+        { name: "Mega Funk", img: "assets/categorias/categoriamegafunk.png" },
+        { name: "trinta", img: "assets/categorias/categoriamegafunk.png" },
+        { name: "Trap", img: "assets/categorias/categoriatrap.png" },
         { name: "Jazz", img: "assets/jazz.jpg" },
         { name: "Hip Hop", img: "assets/hiphop.jpg" }
     ];
+
+
 
     const categoriesElement = document.getElementById("categories");
 
@@ -69,11 +72,14 @@ if (document.getElementById("categories")) {
     });
 }
 
+
+
 // Música de exemplo (em destaque)
 const musicList = [
-    { title: "Música 1", category: "Pop", img: "assets/sla.png", file: "assets/music1.mp3" },
+    { title: "Mega Funk Digo IDG", category: "Mega Funk", img: "assets/categorias/megafunk/musicas/mega_funk_digoidg/megafunkdigoidg.png", file: "assets/categorias/megafunk/musicas/mega_funk_digoidg/megafunkdigoidg.mp3" },
     { title: "Música 2", category: "Rock", img: "assets/music2.jpg", file: "assets/music2.mp3" },
     { title: "Música 3", category: "Jazz", img: "assets/music3.jpg", file: "assets/music3.mp3" },
+    { title: "Música 3", category: "trinta", img: "assets/music3.jpg", file: "assets/music3.mp3" },
     { title: "Música 4", category: "Hip Hop", img: "assets/music4.jpg", file: "assets/music4.mp3" }
 ];
 
@@ -94,7 +100,7 @@ function loadMusicByCategory(category) {
                 <source src="${music.file}" type="audio/mp3">
                 Seu navegador não suporta o elemento de áudio.
             </audio>
-            <button onclick="addToPlaylist('${music.title}')">Adicionar à Playlist</button>
+           
         `;
         musicListElement.appendChild(musicItem);
     });
@@ -103,17 +109,6 @@ function loadMusicByCategory(category) {
 
 
 
-// Função para adicionar música à playlist
-function addToPlaylist(songTitle) {
-    const playlist = JSON.parse(localStorage.getItem("playlist"));
-    if (!playlist.includes(songTitle)) {
-        playlist.push(songTitle);
-        localStorage.setItem("playlist", JSON.stringify(playlist));
-        alert("Música adicionada à playlist!");
-    } else {
-        alert("Música já está na sua playlist.");
-    }
-}
 
 
 // Redirecionamento da categoria para a página de músicas dessa categoria
@@ -122,25 +117,120 @@ if (categoryParam) {
     loadMusicByCategory(categoryParam);
 }
 
-// Função para carregar as últimas atualizações com imagens
-function loadLatestUpdates() {
-    const latestUpdates = document.getElementById("latest-updates");
-    latestUpdates.innerHTML = ""; // Limpar as atualizações antes de adicionar novas
+// Redirecionamento da categoria para a página de músicas dessa categoria
+const gravadoraParam = new URLSearchParams(window.location.search).get("gravadora");
+if (gravadoraParam) {
+    loadMusicBygravadora(gravadoraParam);
+}
 
-    const updates = [
-        { text: "Música 'Música 1' adicionada à categoria Pop.", img: "assets/sla.png" },
-        { text: "Novo álbum 'Músicas 2025' disponível em Jazz.", img: "assets/jazz-icon.jpg" },
-        { text: "Música 'Música 2' lançada no Rock.", img: "assets/rock-icon.jpg" }
+
+// categoria das gtrinta
+if (document.getElementById("trinta")) {
+    const trinta = [
+        { name: "trinta", img: "" },
     ];
 
-    updates.forEach(update => {
-        const updateItem = document.createElement("li");
-        
-        updateItem.innerHTML = `
-            <img src="${update.img}" alt="Ícone de categoria" class="update-img">
-            <span>${update.text}</span>
+    const trintaElement = document.getElementById("trinta");
+
+    trinta.forEach(trinta => {
+        const trintaItem = document.createElement("div");
+        trintaItem.classList.add("trinta-item");
+        trintaItem.innerHTML = `
+            <img src="${trinta.img}" alt="${trinta.name}">
+            <p>${trinta.name}</p>
         `;
-        
-        latestUpdates.appendChild(updateItem);
+        trintaItem.addEventListener("click", () => {
+            window.location.href = `trinta.html?trinta=${trinta.name}`;
+        });
+        trintaElement.appendChild(trintaItem);
     });
 }
+
+
+
+// categoria das gtrinta
+if (document.getElementById("trinta2")) {
+    const trinta2 = [
+        { name: "trinta2", img: "" },
+    ];
+
+    const trinta2Element = document.getElementById("trinta2");
+
+    trinta2.forEach(trinta2 => {
+        const trinta2Item = document.createElement("div");
+        trinta2Item.classList.add("trinta2-item");
+        trinta2Item.innerHTML = `
+            <img src="${trinta2.img}" alt="${trinta2.name}">
+            <p>${trinta2.name}</p>
+        `;
+        trinta2Item.addEventListener("click", () => {
+            window.location.href = `trinta2.html?trinta2=${trinta2.name}`;
+        });
+        trinta2Element.appendChild(trinta2Item);
+    });
+}
+
+
+
+// categoria das gravadoras
+if (document.getElementById("gravadora")) {
+    const gravadora = [
+        { name: "gravadora", img: "gravadoras/trinta/categoria.png" },
+    ];
+
+
+
+    const gravadoraElement = document.getElementById("gravadora");
+
+    gravadora.forEach(gravadora => {
+        const gravadoraItem = document.createElement("div");
+        gravadoraItem.classList.add("gravadora-item");
+        gravadoraItem.innerHTML = `
+            <img src="${gravadora.img}" alt="${gravadora.name}">
+            <p>${gravadora.name}</p>
+        `;
+        gravadoraItem.addEventListener("click", () => {
+            window.location.href = `gravadora.html?gravadora=${gravadora.name}`;
+        });
+        gravadoraElement.appendChild(gravadoraItem);
+    });
+}
+/* ------------------------------------------------------------ */
+
+/*na pagina trinta.html gravadora 30praum */
+
+// Músicas da 30praum
+const trintaList = [
+    { title: "musica1", category: "trinta", img: "assets/categorias/megafunk/musicas/mega_funk_digoidg/megafunkdigoidg.png", file: "assets/categorias/megafunk/musicas/mega_funk_digoidg/megafunkdigoidg.mp3" },
+    { title: "Música 2", category: "trinta", img: "assets/music2.jpg", file: "assets/music2.mp3" },
+    { title: "Música 3", category: "trinta", img: "gravadoras/trinta/categoria.png", file: "" },
+];
+
+
+function loadMusicByCategory(category) {
+    const musicListElement = document.getElementById("trinta-list");
+    musicListElement.innerHTML = ""; // Limpar a lista antes de adicionar novas músicas
+
+    const filteredMusic = trintaList.filter(music => music.category === category);
+    filteredMusic.forEach(music => {
+        const musicItem = document.createElement("div");
+        musicItem.classList.add("music-item");
+        musicItem.innerHTML = `
+            <img src="${music.img}" alt="${music.title}" class="trinta-img">
+            <p>${music.title}</p>
+            <audio controls>
+                <source src="${music.file}" type="audio/mp3">
+                Seu navegador não suporta o elemento de áudio.
+            </audio>
+           
+        `;
+        musicListElement.appendChild(musicItem);
+    });
+}
+// Redirecionamento da categoria para a página de músicas dessa categoria
+const trintaParam = new URLSearchParams(window.location.search).get("trinta");
+if (trintaParam) {
+    loadMusicByCategory(trintaParam);
+}
+
+/* ------------------------------------------------------------ */
